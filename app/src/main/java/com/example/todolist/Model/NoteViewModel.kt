de382eb01881f3f6328b6ebf33eec8e72a7cf2ed
septiarmustafa <septiarmustafa@gmail.com>
2024-03-17 13:05:18 +0700

    Initial Note App

---
+++ app/src/main/java/com/example/todolist/Model/NoteViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.todolist.Model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.todolist.Database.NoteDatabase
+import com.example.todolist.Database.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: NoteRepository
+    val allnotes : LiveData<List<Note>>
+
+    init{
+        val dao = NoteDatabase.getDatabase(application).getNoteDao()
+        repository = NoteRepository(dao)
+        allnotes = repository.allNotes
+    }
+
+    fun deleteNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(note)
+    }
+
+    fun insertNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(note)
+    }
+
+    fun updateNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.update(note)
+    }
+}
---
+++ app/src/main/java/com/example/todolist/utilities/constants.kt
@@ -0,0 +1,3 @@
+package com.example.todolist.utilities
+
+const val DATABASE_NAME = "note_database"
